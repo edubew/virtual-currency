82f9d18781a8e99bc8339c04f8b490651c0f8b20
edubew <edubew98@gmail.com>
2023-02-14 11:51:03 +0300

    Build the  file structure for apps

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navigation = () => (
+  <Header>
+    <div className="logo">
+      <h1>CRYPTO SPACE</h1>
+    </div>
+  </Header>
+);
+
+const Header = styled.header``;
+
+export default Navigation;
