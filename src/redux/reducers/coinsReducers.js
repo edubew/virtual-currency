58d49a0155c92616f08593d8696d9e28b75999b6
edubew <edubew98@gmail.com>
2023-02-14 23:50:50 +0300

    Set the initial state of coins to an empty array

---
+++ src/redux/reducers/coinsReducers.js
@@ -0,0 +1,8 @@
+import { GET_COINS, COIN_DETAILS } from "../actions/coinActions";
+import { fetchCoins, fetchCoinsDetails } from "../api/coinsApi";
+
+const initialState = { coinsState: [], dataState: [] };
+
+export const coinsReducers = (state = initialState, action) => {
+  sw
+}
