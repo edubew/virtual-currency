6644b4461ddf0c3dc60c3a3480621b8de14ad1b6
edubew <edubew98@gmail.com>
2023-02-14 21:37:14 +0300

    Create the application's header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,9 +6,27 @@ const Navigation = () => (
     <div className="logo">
       <h1>CRYPTO SPACE</h1>
     </div>
+    <div className="search__bar">
+      <input type="text" placeholder="search coin..." />
+    </div>
   </Header>
 );
 
-const Header = styled.header``;
+const Header = styled.header`
+display: flex;
+justify-content: space-between;
+background-color: #001b48;
+padding: 2rem 3rem;
+
+.search__bar {
+  input {
+    padding: 12px;
+    border-radius: 20px;
+    font-size: 1rem;
+    background-color: transparent;
+    border: 1px solid gray;
+  }
+}
+`;
 
 export default Navigation;
