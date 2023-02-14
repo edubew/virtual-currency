620aaad4854e1f96bbc351da91c485b844c31a75
edubew <edubew98@gmail.com>
2023-02-15 00:01:50 +0300

    Write reducers for the redux store

--- src/redux/actions/coinActions.js
+++ src/redux/actions/coinActions.js
@@ -1,2 +1,2 @@
 export const GET_COINS = 'GET_COINS';
-export const COIN_DETAILS = 'COIN_DETAILS';
+export const GET_COIN_DETAILS = 'COIN_DETAILS';
--- src/redux/reducers/coinsReducers.js
+++ src/redux/reducers/coinsReducers.js
@@ -1,8 +1,21 @@
-import { GET_COINS, COIN_DETAILS } from "../actions/coinActions";
-import { fetchCoins, fetchCoinsDetails } from "../api/coinsApi";
+import { GET_COINS, GET_COIN_DETAILS } from '../actions/coinActions';
+import { fetchCoins, fetchCoinsDetails } from '../api/coinsApi';
 
 const initialState = { coinsState: [], dataState: [] };
 
 export const coinsReducers = (state = initialState, action) => {
-  sw
-}
+  switch (action.type) {
+    case GET_COINS:
+      return {
+        ...state,
+        coinsState: [...action.payload],
+      };
+    case GET_COIN_DETAILS:
+      return {
+        ...state,
+        dataState: [...action.payload],
+      };
+    default:
+      return state;
+  }
+};
