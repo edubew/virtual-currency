ce064e219f4c195410be76e12067cb2793f65476
edubew <edubew98@gmail.com>
2023-02-18 09:27:25 +0300

    Add styling to the coindetails section

--- src/components/CoinDetails.js
+++ src/components/CoinDetails.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { getCoinDetails } from '../redux/reducers/coinsReducers';
+import currency from '../assets/currency.jpg';
 
 const CoinDetails = () => {
   const params = useParams();
@@ -15,70 +17,110 @@ const CoinDetails = () => {
   }, [id, dispatch]);
 
   return (
-  // <div>
-  //   <div>
-  //     <a href="/details" onChange={(e) => setData(e.target.value)}>
-  //       <img src={coins.image} alt="coin" />
-  //     </a>
-  //     <div>
-  //       <h3>{coins.name}</h3>
-  //       <p>{coins.symbol}</p>
-  //       <h4>{coins.market_cap}</h4>
-  //     </div>
-  //   </div>
-
-    //   <div>
-    //     <div>
-    //       <p>
-    //         {name}
-    //         {' '}
-    //         Breakdown
-    //       </p>
-    //     </div>
-    //     <table>
-    //       <tbody>
-    //         {coins.map((coin) => (
-    //           <tr key={coin.id}>
-    //             <td>{coin.market_cap}</td>
-    //           </tr>
-    //         ))}
-    //       </tbody>
-    //     </table>
-    //   </div>
-    // </div>
-    <div>
+    <Div>
       {coins === [] ? (
         <p>Loading...</p>
       ) : (
         <div>
-          <img
-            src="https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579"
-            alt="coin"
-          />
-          <h3>{coins.name}</h3>
-          <p>{coins.symbol}</p>
+          <div className="header__container">
+            <img src={currency} alt="coin" />
+            <div>
+              <h3>{coins.name}</h3>
+              <p>{coins.symbol}</p>
+            </div>
+          </div>
+
+          <div className="table__details">
+            <p>Coin Details</p>
+            <table key={coins.id}>
+              <tbody>
+                <tr>
+                  <td>Market Cap Rank</td>
+                  <td>{coins.market_cap_rank}</td>
+                </tr>
+                <tr>
+                  <td>Coin Id</td>
+                  <td>{coins.id}</td>
+                </tr>
+                <tr>
+                  <td>Block Time in Minutes</td>
+                  <td>{coins.block_time_in_minutes}</td>
+                </tr>
+                <tr>
+                  <td>Hashing Algorithm</td>
+                  <td>{coins.hashing_algorithm}</td>
+                </tr>
+                <tr>
+                  <td>Genesis Date</td>
+                  <td>{coins.genesis_date}</td>
+                </tr>
+                <tr>
+                  <td>Liquidity Score</td>
+                  <td>{coins.liquidity_score}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
-
-      {/* <div>
-        <div>
-          <p>
-            {name}
-            {' '}
-            Breakdown
-          </p>
-        </div>
-        <table>
-          <tbody>
-              <tr key={coin.id}>
-                <td>{coin.market_cap}</td>
-                   </tr>
-            ))}
-          </tbody>
-        </table>
-      </div> */}
-    </div>
+    </Div>
   );
 };
 
 export default CoinDetails;
+
+const Div = styled.div`
+  .header__container {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    img {
+      width: 24rem;
+
+      @media screen and (max-width: 600px) {
+        width: 12rem;
+      }
+      
+    }
+    h3 {
+      font-size: 1.8rem;
+      font-weight: bolder;
+    }
+  }
+
+  .table__details {
+    p {
+      text-align: center;
+      font-size: 1.5rem;
+      font-weight: bolder;
+    }
+
+    table {
+      border-colapse: collapse;
+      width: 80%;
+      margin: 0 10%;
+
+      @media screen and (max-width: 600px) {
+        width: 100%;
+        margin: 0;
+      }
+    }
+
+    td {
+      padding: 8px;
+    }
+
+    tr {
+      padding: 8px;
+      font-size: 1.2rem;
+      background-color: #4167ad;
+    }
+
+    tr:nth-child(even) {
+      background-color: #3d60a2;
+    }
+  }
+`;
